db234aef2bc22b03382a7047b96b5ade2540828c
Allston Lee Mickey <amickey@unix3.lt.ucsc.edu>
2017-04-18 21:29:59 -0700

    Cleaned up program structure by merging argc == 1 case with argc > 1 case

--- lab1/cat.c
+++ lab1/cat.c
@@ -6,62 +6,63 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
-	if (argc == 1) // if there is only 1 argument
+	int arg_i;
+	if (argc == 1) // if there's only one argument
 	{
-		char* buf[2048];
-		ssize_t bytesRead;
-		while ((bytesRead = read(STDIN_FILENO, buf, 2048)) > 0) // keep getting input and writing to standard output until the user quits
+		arg_i = 0; // set the index to 0, only read input from keyboard
+	}
+	else // more than one argument -> try to read & write each file in argv
+	{
+		arg_i = 1; // the first file has index 1 in argv
+	}
+
+	while (argv[arg_i] != NULL) // while there is given input
+	{
+		int fd;
+		if (arg_i == 0)
 		{
-			ssize_t bytesWritten = write(STDOUT_FILENO, buf, bytesRead);
-			if (bytesWritten == -1)
-			{
-				perror(argv[0]);
-				exit(errno);
-			}
+			fd = STDIN_FILENO;
 		}
-		if (bytesRead == -1)
+		else
+		{
+			fd = open(argv[arg_i], O_RDONLY); // open it as read only
+		}
+
+		if (fd == -1)
 		{
 			perror(argv[0]);
-			exit(errno);
 		}
-	}
-	else
-	{
-		for (int arg_i = 1; arg_i < argc; arg_i++) // for each file
+		else // file opened successfully
 		{
-			int fd = open(argv[arg_i], O_RDONLY); // open it as read only
-			if (fd == -1)
+			// read the file into a string buffer, 2048 bytes at a time, then write from the buffer.
+			char* buf[2048]; // 2048 byte string buffer
+			ssize_t bytesRead;
+			while ((bytesRead = read(fd, buf, 2048)) > 0)
+			{
+				ssize_t bytesWritten = write(STDOUT_FILENO, buf, bytesRead);
+				if (bytesWritten == -1)
+				{
+					perror(argv[0]);
+				}
+			}
+			if (bytesRead == -1)
 			{
 				perror(argv[0]);
 			}
-			else // file opened successfully
-			{
-				// read the file into a string buffer, 2048 bytes at a time, then write from the buffer.
-				char* buf[2048]; // 2048 byte string buffer
-				ssize_t bytesRead;
-				while ((bytesRead = read(fd, buf, 2048)) > 0)
-				{
-					ssize_t bytesWritten = write(STDOUT_FILENO, buf, bytesRead);
-					if (bytesWritten == -1)
-					{
-						perror(argv[0]);
-					}
-				}
-				if (bytesRead == -1)
-				{
-					perror(argv[0]);
-				}
 
-				int closed = close(fd);
-				if (closed == -1)
+			if (fd != STDIN_FILENO) // if a file was read
+			{
+				if (close(fd) == -1)
 				{
 					perror(argv[0]);
 				}
 			}
 		}
+		++arg_i; // go onto the next file
 	}
 
 	return 0;
