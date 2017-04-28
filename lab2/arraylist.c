22e3ed0a62e309dc9eecdf7cd230c36c309a5a2c
Allston Lee Mickey <amickey@unix2.lt.ucsc.edu>
2017-04-28 07:47:13 -0700

    Changed cap to 8 ints.  Added some comments.

--- lab2/arraylist.c
+++ lab2/arraylist.c
@@ -5,9 +5,14 @@
 
 int main(void)
 {
-	uint32_t cap = 1;
-	uint32_t len = 0;
+	uint32_t cap = 8; // max number of integers to store
+	uint32_t len = 0; // current number of integers stored
 	uint32_t *nums = (uint32_t *) calloc(cap, sizeof(uint32_t));
+	if (nums == NULL)
+	{
+		printf("Calloc failure: {nums == NULL} [%d]\n", errno);
+		return errno;
+	}
 
 	char action = 0;
 	while (action != 'Q')
@@ -16,7 +21,7 @@ int main(void)
 		scanf(" %c", &action);
 		switch (action)
 		{
-			case 'I':
+			case 'I': // store an int in an array, reallocate double memory if needed
 			{
 				uint32_t num;
 				printf("Enter an integer: ");
@@ -28,7 +33,7 @@ int main(void)
 					temp = (uint32_t *) realloc(nums, cap * sizeof(uint32_t));
 					if (temp == NULL)
 					{
-						printf("realloc error: {temp == NULL} [%d]\n", errno);
+						printf("Realloc failure: {temp == NULL} [%d]\n", errno);
 					}
 					else
 					{
@@ -38,7 +43,7 @@ int main(void)
 				nums[len++] = num;
 				break;
 			}
-			case 'P':
+			case 'P': // print the stored ints
 			{
 				printf("Length = %u:", len);
 				if (len)
@@ -51,16 +56,19 @@ int main(void)
 				printf("\n");
 				break;
 			}
-			case 'Q':
+			case 'Q': // do nothing and break out --> stop asking for input
 			{
 				break;
 			}
-			default:
+			default: // do nothing and break out --> ask for more input
 			{
 				break;
 			}
 		}
 	}
+
 	free(nums);
+	nums = NULL;
+
 	return 0;
 }
