68006724f1408ccf453bc416b8689e3bbf70f240
Allston Lee Mickey <amickey@unix3.lt.ucsc.edu>
2017-04-29 00:29:11 -0700

    Added flag parsing for -p, -n, and -r.

--- assignment2/sorting.c
+++ assignment2/sorting.c
@@ -1,11 +1,48 @@
 # include <stdio.h> // printf, perror
 # include <stdlib.h> // atoi, rand & srand, malloc/calloc/realloc/free
-# include <unistd.h> // getopt
+# include <getopt.h> // getopt
 # include "bv.h"
 # include "minsort.h"
 
 int main(int argc, char *argv[])
 {
+	int printNum = 100, seed = 8222022, capacity = 100;
 	
+	int opt;
+	while ((opt = getopt(argc, argv, "AmbiqMp:r:n:")) != -1)
+	{
+		switch (opt)
+		{
+			case 'p':
+			{
+				printNum = atoi(optarg);
+				break;
+			}
+			case 'r':
+			{
+				seed = atoi(optarg);
+				break;
+			}
+			case 'n':
+			{
+				capacity = atoi(optarg);
+				break;
+			}
+			case '?':
+			{
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+	}
+
+	printf("printNum: %d\n", printNum);
+	printf("seed: %d\n", seed);
+	printf("capacity: %d\n", capacity);
+
 	return 0;
 }
+
