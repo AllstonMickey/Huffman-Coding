e495c20895cc72508af108bebf40e29218bb9d11
Allston Lee Mickey <amickey@unix2.lt.ucsc.edu>
2017-04-29 17:03:58 -0700

    Cleaned up main()

--- assignment2/sorting.c
+++ assignment2/sorting.c
@@ -6,10 +6,12 @@
 
 enum sortingAlgorithms { MIN, BUBBLE, INSERTION, QUICK, MERGE, END };
 
+void printArray(uint32_t a[], uint32_t printLength);
+
 int main(int argc, char *argv[])
 {
-	int printNum = 100, seed = 8222022, capacity = 100, length = 0;
-	bitV *sorts = newVec(6);
+	uint32_t printNum = 100, seed = 8222022, capacity = 100, length = 0;
+	bitV *sorts = newVec(8);
 
 	int opt;
 	while ((opt = getopt(argc, argv, "AmbiqMp:r:n:")) != -1)
@@ -46,10 +48,14 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-	
+
+	/*
+	* Create a dynamic array of random 24 bit integers.
+	*/	
+
 	srand(seed);
 	uint32_t *nums = (uint32_t *) calloc(capacity, sizeof(uint32_t));	
-	for (int i = 0; i < capacity; ++i)
+	for (uint32_t i = 0; i < capacity; i += 1)
 	{
 		if (length == capacity)
 		{
@@ -59,16 +65,14 @@ int main(int argc, char *argv[])
 		length += 1;
 	}
 
-	for (int i = 1; i <= printNum; ++i)
-	{
-		int columnWidth = 8;
-		printf("  %*d", columnWidth, nums[i-1]);
-		if (i % 7 == 0)
-		{
-			printf("\n");
-		}
-	}
+	/*
+	* Sort the array.
+	* Print the array.
+	*/
 
+	minSort(nums, length);	
+	printArray(nums, printNum);
+	
 	/*
 	* Currently prints out an unsorted array of 24 bit integers
 	* Next: sort the array with minsort and reallocate memory to expand array
@@ -80,3 +84,16 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+void printArray(uint32_t a[], uint32_t length)
+{
+	for (uint32_t i = 0; i < length; i += 1)
+	{
+		int columnWidth = 8;
+		printf("  %*d", columnWidth, a[i]);
+
+		if ((i+1) % 7 == 0) // print a new line after every 7 entries
+		{
+			printf("\n");
+		}
+	}
+}
