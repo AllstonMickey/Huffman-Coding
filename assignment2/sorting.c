ecf026f5201324c7aa64325763befac1c59a3359
Allston Lee Mickey <amickey@unix3.lt.ucsc.edu>
2017-05-02 12:30:58 -0700

    Prevented printing of numbers when array size was smaller than the print count.

--- assignment2/sorting.c
+++ assignment2/sorting.c
@@ -1,16 +1,17 @@
 # include <stdio.h> // printf, perror
 # include <stdlib.h> // atoi, rand & srand, malloc/calloc/realloc/free
 # include <getopt.h> // getopt
+# include <math.h>
 # include "bv.h"
 # include "minsort.h"
 
 enum sortingAlgorithms { MIN, BUBBLE, INSERTION, QUICK, MERGE, END };
 
-void printArray(uint32_t a[], uint32_t printLength);
+void printArray(uint32_t a[], uint32_t len, uint32_t printAmount);
 
 int main(int argc, char *argv[])
 {
-	uint32_t printNum = 100, seed = 8222022, capacity = 100, length = 0;
+	uint32_t printNum = 100, seed = 8062022, capacity = 100, length = 0;
 	bitV *sorts = newVec(8);
 
 	int opt;
@@ -64,14 +65,14 @@ int main(int argc, char *argv[])
 		nums[i] = rand() & MASK;
 		length += 1;
 	}
-
+	
 	/*
 	* Sort the array.
 	* Print the array.
 	*/
 
-	minSort(nums, length);	
-	printArray(nums, printNum);
+	//minSort(nums, length);	
+	printArray(nums, length, printNum);
 	
 	/*
 	* Currently prints out an unsorted array of 24 bit integers
@@ -84,9 +85,18 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-void printArray(uint32_t a[], uint32_t length)
+/*
+ * printArray: prints each number in a[] up to a printAmount or len of the array
+ *	to print all elements of the array, make printAmount = length
+ *
+ * @param   a            The array of integers to print
+ * @param   len          The number of elements in the array
+ * @param   printAmount  The number of elements to print
+ * @return  void
+ */
+void printArray(uint32_t a[], uint32_t len, uint32_t printAmount)
 {
-	for (uint32_t i = 0; i < length; i += 1)
+	for (uint32_t i = 0; i < printAmount && i < len; i += 1)
 	{
 		int columnWidth = 8;
 		printf("  %*d", columnWidth, a[i]);
@@ -97,3 +107,4 @@ void printArray(uint32_t a[], uint32_t length)
 		}
 	}
 }
+
