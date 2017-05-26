229dc491ed51f812c9e24c379dc3ae4b9b153bdd
Allston Lee Mickey <amickey@unix4.lt.ucsc.edu>
2017-05-25 17:58:45 -0700

    Changed interface of queue to be that of a binary heap.

--- assignment4/queue.c
+++ assignment4/queue.c
@@ -1,3 +1,7 @@
+/*
+ * Implementation of a priority queue as a binary min-heap
+ */
+
 # include <stdlib.h>
 # include "queue.h"
 
@@ -5,24 +9,15 @@
 # define NIL (void *) 0
 # endif
 
-/* typedef struct queue
- * {
- * 	uint32_t size;       // How big is it?
- * 	uint32_t head, tail; // Front and rear locations
- *	item *entries;       // Array of entries
- * } queue;
- */
-
 queue *newQueue(uint32_t size)
 {
 	queue *q = (queue *) malloc(sizeof(queue));
 	if (q)
 	{
-		q->entries = (queueItem *) calloc(size, sizeof(queueItem));
-		if (q->entries)
+		q->nodes = (queueItem *) calloc(size, sizeof(queueItem));
+		if (q->nodes)
 		{
 			q->size = size;
-			q->head = q->tail = 0;
 			return q;
 		}
 	}
@@ -31,45 +26,17 @@ queue *newQueue(uint32_t size)
 
 void delQueue(queue *q)
 {
-	free(q->entries);
-	q->entries = NIL;
+	free(q->nodes);
+	q->nodes = NIL;
 	free(q);
 	q = NIL;
 }
 
 /*
- * Adds an entry to the front.
- *
- * @param q Queue to add to
- * @param i Entry to add
- * @return true  Success
- * 	   false Failure
+ * Adds an item to the queue
  */
 bool enqueue(queue *q, queueItem i)
 {
-
-}
-
-/*
- * Removes the rear entry (smallest value).
- *
- * @param q Queue to remove from
- * @param i Item to set to the removed entry
- * @return true  Success
- * 	   false Failure
- */
-bool dequeue(queue *q, queueItem *i)
-{
-
-}
-
-bool empty(queue *q)
-{
-
-}
-
-bool full(queue *q)
-{
-
+	
 }
 
--- assignment4/queue.h
+++ assignment4/queue.h
@@ -11,25 +11,12 @@ typedef QUEUE_ITEM queueItem;
 
 typedef struct queue
 {
-	uint32_t size;       // How big is it? (number of entries)
-	uint32_t head, tail; // Front and rear locations
-	queueItem *entries;       // Array to hold the entries
+	uint32_t size;    // How big is it? (number of entries)
+	queueItem *nodes; // Array of nodes for the heap
 } queue;
 
 queue *newQueue(uint32_t size);
 
 void delQueue(queue *q);
-
-// Adds an entry to the front
-bool enqueue(queue *q, queueItem i);
-
-// Removes the rear entry (smallest value) and sets i to it
-bool dequeue(queue *q, queueItem *i);
-
-// Is it empty?
-bool empty(queue *q);
-
-// Is it full?
-bool full(queue *q);
 
 # endif
