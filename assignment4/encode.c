392a120d53ce9e14eac4ab10cf0a8055f3d03b32
Allston Lee Mickey <amickey@unix4.lt.ucsc.edu>
2017-05-29 22:32:28 -0700

    Added histogram to encode.c.

--- assignment4/encode.c
+++ assignment4/encode.c
@@ -7,9 +7,11 @@
 # include "huffman.h"   // huffman trees, stacks, stdint, stdbool
 # include "queue.h"     // queues, heaps, stdint, stdbool
 
+void populateHistogram(char *file, uint32_t hist[]);
+
 int main(int argc, char **argv)
 {
-	char *in;
+	char *in = NIL;
 	char *out = NIL;
 	bool verbose = false;
 
@@ -63,4 +65,31 @@ int main(int argc, char **argv)
 	dequeue(q, &res[2]);
 	printQueue(q);
 	*/
+
+	in = "/afs/cats.ucsc.edu/users/g/darrell/encode";
+	uint32_t histogram[256] = { 0 };
+	histogram[0] = 1;
+	histogram[255] = 1;
+	populateHistogram(in, histogram);
+}
+
+/*
+ * Counts the number of occurrences of each byte in a file,
+ * storing them in a histogram.
+ *
+ * @param file Path to the file to read from
+ * @param hist Array to store the occurrences
+ */
+void populateHistogram(char *file, uint32_t hist[])
+{
+	int fd = open(file, O_RDONLY);
+	struct stat buffer;
+	fstat(fd, &buffer);
+
+	bitV *v = newVec(buffer.st_size * 8);
+	int64_t n = read(fd, v->v, buffer.st_size);
+	for (uint64_t i = 0; i < buffer.st_size; i += 1)
+	{
+		hist[v->v[i]] += 1;
+	}
 }
