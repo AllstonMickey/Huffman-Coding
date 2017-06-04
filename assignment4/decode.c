38af8ea3e8fcbf2ab62dea27108ea58322fd5b39
Allston Lee Mickey <amickey@unix4.lt.ucsc.edu>
2017-06-03 20:34:12 -0700

    Read magic number and added error handling.

--- assignment4/decode.c
+++ assignment4/decode.c
@@ -1,7 +1,122 @@
+# include <sys/types.h> // open
+# include <sys/stat.h>  // open
+# include <fcntl.h>     // open
+# include <unistd.h>    // read
+# include <getopt.h>
+# include <string.h>
+# include "bv.h"
 # include "stack.h"
 
-int main(void)
+# ifndef MAX_BUF
+# define MAX_BUF 128
+# endif
+
+# ifndef MAGIC_NUM
+# define MAGIC_NUM 0xdeadd00d
+# endif
+
+uint64_t readSFile(char *file);
+
+int main(int argc, char **argv)
 {
-	
+	char in[MAX_BUF] = {'\0'};
+	char out[MAX_BUF] = {'\0'};
+
+	bool verbose = false;
+	bool pFlag = false;
+	bool cFlag = false;
+
+	int opt;
+	while ((opt = getopt(argc, argv, "i:o:Avpc")) != -1)
+	{
+		switch (opt)
+		{
+			case 'i':
+				{
+					strncpy(in, optarg, sizeof(char) * MAX_BUF);
+					// strncpy does not null-term strings if the buf is maxed in size
+					if (in[MAX_BUF - 1] != '\0')
+					{
+						in[MAX_BUF - 1] = '\0';
+					}
+					break;
+				}
+			case 'o':
+				{
+					strncpy(out, optarg, sizeof(char) * MAX_BUF);
+					// strncpy does not null-term strings if the buf is maxed in size
+					if (out[MAX_BUF - 1] != '\0')
+					{
+						out[MAX_BUF - 1] = '\0';
+					}
+					break;
+				}
+			case 'A':
+				{
+					verbose = true;
+					pFlag = true;
+					cFlag = true;
+					break;
+				}
+			case 'v':
+				{
+					verbose = true;
+					break;
+				}
+			case 'p':
+				{
+					pFlag = true;
+					break;
+				}
+			case 'c':
+				{
+					cFlag = true;
+					break;
+				}
+			case '?':
+				{
+					break;
+				}
+			default:
+				{
+					break;
+				}
+		}
+	}
+
+	while (in[0] == '\0')
+	{
+		printf("Enter an input file path: ");
+		scanf("%s", in);
+	}
+
+	printf("%u\n", readSFile(in));
+
 	return 0;
 }
+
+uint64_t readSFile(char *file)
+{
+	int fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Cannot open input file");
+		return 0;
+	}
+
+	uint32_t magicNumber;
+	read(fd, &magicNumber, sizeof(magicNumber));
+
+	printf("Magic number: %u\n", magicNumber);
+
+	if (magicNumber != MAGIC_NUM)
+	{
+		printf("Wrong magic number.  Exiting\n");
+		close(fd);
+		return 1;
+	}
+
+	printf("Success!\n");
+	return 2;
+}
+
